9f7de024fa5656b1e986068a1c32e3e14114df4f
Dianna Faulk <diannafaulk@gmail.com>
2017-01-25 10:49:46 -0800

    Include Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// Foreign Modules
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
+// Components
 import App from './components/App/App';
 import NotFound from './components/NotFound/NotFound';
+// Style
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 const Root = () => {
